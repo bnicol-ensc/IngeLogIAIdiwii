91f95a5c023674f8502dcb5c770b32787fb9449f
Guillaume Grosse <45588739+guiguite38@users.noreply.github.com>
2020-12-17 16:49:09 +0100

    model + api

---
+++ tests/api.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import spacy 
+
+app = Flask(__name__)
+@app.route('/')
+def hello():
+    return {'Yolo' : 'Swag'}
+
+
+@app.route('/api/<sentence>')
+def hello_world(sentence):
+    # texts = ["Are you ready for the tea party????? It's gonna be wild"]
+    docs = nlp.tokenizer(sentence)
+
+    # Use textcat to get the scores for each doc
+    scores, _ = textcat.predict([docs])
+    label = scores.argmax()
+    textcat.labels[label]
+
+    print('poule up')
+    return textcat.labels[label]
+    
+
+# //
+
+# sentence="manger chinois"
+# docs = nlp.tokenizer(sentence)
+# scores, _ = textcat.predict([docs])
+# label = scores.argmax()
+# textcat.labels[label]
+
+# print(textcat.labels[label])
+
+# //
+
+if __name__ == "__main__":
+    nlp = spacy.load("./models/model1")
+    textcat = nlp.get_pipe('textcat')
+
+    app.run(debug=True)
